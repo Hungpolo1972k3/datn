6a9bc84e041dd9fe4f332596c91466c1df336ede
Hung <hungnguyen1972k3@gmail.com>
2025-04-02 00:29:12 +0700

    update

--- services/amr.js
+++ services/amr.js
@@ -32,31 +32,26 @@ const runAmrFinder = (filePath, res) => {
 };
 
 const runAmrFinderString = (nucleicString, res) => {
-    if (!nucleicString || nucleicString.trim() === "") {
-        return res.status(400).json({ error: "Chuỗi nucleic không hợp lệ hoặc trống" });
-    }
-
     const fastaContent = `>temp_sequence\n${nucleicString.replace(/\n/g, "").trim()}`;
     const tempFastaFilePath = path.resolve(__dirname, "temp_input.fasta");
     fs.writeFileSync(tempFastaFilePath, fastaContent);
-    
     const outputFilePath = `${tempFastaFilePath}_amrfinder.tsv`;
     const command = `amrfinder -n ${tempFastaFilePath} -o ${outputFilePath}`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
-            throw new Error("Lỗi: " + error.message);
+            console.error("AMRFinder error:", stderr); 
+            return res.status(500).json({ error: "Lỗi khi chạy AMRFinder" });
         }
         fs.readFile(outputFilePath, "utf8", (err, data) => {
             removeFiles([tempFastaFilePath, outputFilePath]);
             if (err) {
-                throw new Error("Lỗi: " + err.message);
+                return res.status(500).json({ error: "Lỗi khi đọc kết quả AMRFinder" });
             }
             res.json({ result: data });
         });
     });
 };
-
 module.exports = {
     runAmrFinder,
     runAmrFinderString
