d89eaf0294d4bf913c3690364f0e027ec7d1a8e0
Hung <hungnguyen1972k3@gmail.com>
2025-04-01 20:43:41 +0700

    update

--- services/virulence.js
+++ services/virulence.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
 const removeFiles = (files) => {
     files.forEach((file) => {
