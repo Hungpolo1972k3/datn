d598943e57ba50b6018a2f3d62c3ba68222fc597
Hung <hungnguyen1972k3@gmail.com>
2025-04-16 01:14:56 +0700

    update docker

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
