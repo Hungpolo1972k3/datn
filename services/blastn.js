863644a476e0a274cd3a567308c4ddb4a0608200
Hung <hungnguyen1972k3@gmail.com>
2025-05-14 17:23:20 +0700

    update docker

--- services/blastn.js
+++ services/blastn.js
@@ -68,13 +68,13 @@ const runBlastn = async (queryFastaPath, res) => {
 
                     let subjectFileContent = '';
                     try {
-                        subjectFileContent = fs.readFile(subjectPath, 'utf8');
+                        subjectFileContent = await fs.promises.readFile(subjectPath, 'utf8');
                     } catch (err) {
                         console.error(`Error reading subject file: ${err.message}`);
                     }
                     let queryFileContent = '';
                     try {
-                        queryFileContent = fs.readFile(queryPath, 'utf8');
+                        queryFileContent = await fs.promises.readFile(queryPath, 'utf8');
                     } catch (err) {
                         console.error(`Error reading query file: ${err.message}`);
                     }
