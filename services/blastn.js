25b2f38abb9365831012812580913519188d4c2b
Hung <hungnguyen1972k3@gmail.com>
2025-05-16 13:59:55 +0700

    update run blastn tool

--- services/blastn.js
+++ services/blastn.js
@@ -58,7 +58,7 @@ const parseBlastResults = (blastText, querySeq, subjectSeq) => {
 const runBlastn = async (queryFastaPath, res) => {
     const queryPath = path.resolve(queryFastaPath);
     const { default: pLimit } = await import('p-limit');
-    const limit = pLimit(10);
+    const limit = pLimit(4);
     const querySeq = await readFastaSequence(queryPath);
 
     try {
