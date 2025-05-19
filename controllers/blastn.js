fb0592008afd4830103f8cbc23c0ad74f894a07c
Hung <hungnguyen1972k3@gmail.com>
2025-05-20 00:13:53 +0700

    update blastn 195

--- controllers/blastn.js
+++ controllers/blastn.js
@@ -1,10 +1,11 @@
 const blastnService = require("../services/blastn");
 
 const runBlastn = async(req, res) => {
+    const {id} = req.params;
     if (!req.file) {
         return res.status(400).json({ error: "No file uploaded" });
     }
-    await blastnService.runBlastn(req.file.path, res);
+    await blastnService.runBlastn(req.file.path, id, res);
 };
 
 module.exports = {
