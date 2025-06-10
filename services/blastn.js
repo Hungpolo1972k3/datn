d3294280e6a0390ee8336a0e4708184187707a66
Hung <hungnguyen1972k3@gmail.com>
2025-06-10 22:49:27 +0700

    update blastn

--- services/blastn.js
+++ services/blastn.js
@@ -25,91 +25,89 @@ const runBlastnTool = async (inputFilePath, filename, id) => {
     //   }
     // );
     const fastaContent = await fs.promises.readFile(inputFilePath, 'utf8');
-    // const virulence = await axios.post(`${process.env.BIOTOOL_URL}/api/virulence/abricate`, form, {
-    //   headers: {
-    //     ...form.getHeaders(),
-    //   },
-    // });
-    // const virulenceList = await virulenceService.changleVirulenceInfo(virulence.data.result);
-    // const fastaData = virulenceService.parseFasta(fastaContent);
-    // const virulenceDocs = await Promise.all(virulenceList.map(async (v) => {
-    //         const seq = fastaData[v.sequence];
-    //         const rawSeq = seq ? seq.substring(v.start - 1, v.stop) : null;
-    //         const nucleic = rawSeq ? (v.strand === '-' ? virulenceService.reverseComplement(rawSeq) : rawSeq) : null;
-    //         const productInfo = virulenceService.parseProductInfo(v.product);
-    //         return {
-    //             sequence: v.sequence,
-    //             start: v.start,
-    //             stop: v.stop,
-    //             strand: v.strand,
-    //             gene: v.gene,
-    //             coverage: v.coverage,
-    //             identity: v.identity,
-    //             accession: v.accession,
-    //             database: v.database,
-    //             nucleic,
-    //             resistance: v.resistance,
-    //             description: productInfo.description,
-    //             group: productInfo.group,
-    //             vfdb_id: productInfo.vfdb_id,
-    //             function_group: productInfo.function_group,
-    //             function_group_id: productInfo.function_group_id,
-    //         };
-    //     }));
-    // return virulenceDocs;
-    const response = await axios.post(`${process.env.BIOTOOL_URL}/api/amrfinder/amrfinder`, form, {
+    const virulence = await axios.post(`${process.env.BIOTOOL_URL}/api/virulence/abricate`, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
+    const virulenceList = await virulenceService.changleVirulenceInfo(virulence.data.result);
+    const fastaDataVirulence = virulenceService.parseFasta(fastaContent);
+    const virulenceDocs = await Promise.all(virulenceList.map(async (v) => {
+      const seq = fastaDataVirulence[v.sequence];
+      const rawSeq = seq ? seq.substring(v.start - 1, v.stop) : null;
+      const nucleic = rawSeq ? (v.strand === '-' ? virulenceService.reverseComplement(rawSeq) : rawSeq) : null;
+      const productInfo = virulenceService.parseProductInfo(v.product);
+      return {
+        sequence: v.sequence,
+        start: v.start,
+        stop: v.stop,
+        strand: v.strand,
+        gene: v.gene,
+        coverage: v.coverage,
+        identity: v.identity,
+        accession: v.accession,
+        database: v.database,
+        nucleic,
+        resistance: v.resistance,
+        description: productInfo.description,
+        group: productInfo.group,
+        vfdb_id: productInfo.vfdb_id,
+        function_group: productInfo.function_group,
+        function_group_id: productInfo.function_group_id,
+        };
+    }));
+    const amr = await axios.post(`${process.env.BIOTOOL_URL}/api/amrfinder/amrfinder`, form, {
       headers: {
         ...form.getHeaders(),
         },
     });
             
-    const fastaData = amrService.parseFasta(fastaContent);
-    const amrList = amrService.changeAmrInfo(response.data.result);
+    const fastaDataAmr = amrService.parseFasta(fastaContent);
+    const amrList = amrService.changeAmrInfo(amr.data.result);
     
     const amrDocs = amrList.map((v) => {
-      const seq = fastaData[v.contig_id];
+      const seq = fastaDataAmr[v.contig_id];
       const rawSeq = seq ? seq.substring(v.start - 1, v.stop) : null;
       const nucleic = rawSeq ? (v.strand === '-' ? amrService.reverseComplement(rawSeq) : rawSeq) : null;
-                return {
-                    protein_identifier: v.protein_identifier,
-                    contig_id: v.contig_id,
-                    start: v.start,
-                    stop: v.stop,
-                    strand: v.strand,
-                    gene_symbol: v.gene_symbol,
-                    element_name: v.element_name,
-                    closest_reference_name: v.closest_reference_name,
-                    scope: v.scope,
-                    element_type: v.element_type,
-                    class: v.class,
-                    subclass: v.subclass,
-                    method: v.method,
-                    length: v.length,
-                    reference_length: v.reference_length,
-                    alignment_length: v.alignment_length,
-                    coverage: v.coverage,
-                    identity: v.identity,
-                    accession: v.accession,
-                    nucleic
-                };
-            });
-      return amrDocs;
-    // const amr = await amrService.runAmrTool2(inputFilePath);
-    // const resultObject = {
-    //   virulence: virulence,
-    //   amr: amr
-    // };
-    // const resultJson = JSON.stringify(resultObject, null, 2);
-    // const fastaDir = path.join('/app', 'Blastn');
-    // const outputGzPath = path.join(fastaDir, `${id}_result.json.gz`);
+      return {
+        protein_identifier: v.protein_identifier,
+        contig_id: v.contig_id,
+        start: v.start,
+        stop: v.stop,
+        strand: v.strand,
+        gene_symbol: v.gene_symbol,
+        element_name: v.element_name,
+        closest_reference_name: v.closest_reference_name,
+        scope: v.scope,
+        element_type: v.element_type,
+        class: v.class,
+        subclass: v.subclass,
+        method: v.method,
+        length: v.length,
+        reference_length: v.reference_length,
+        alignment_length: v.alignment_length,
+        coverage: v.coverage,
+        identity: v.identity,
+        accession: v.accession,
+        nucleic
+      };
+    });
+    const resultObject = {
+      virulence: virulenceDocs,
+      amr: amrDocs
+    };
+    const resultJson = JSON.stringify(resultObject, null, 2);
+    const fastaDir = path.join('/app', 'Blastn');
+    const outputGzPath = path.join(fastaDir, `${id}_result.json.gz`);
 
-    // await new Promise((resolve, reject) => {
-    //   const gzip = zlib.createGzip();
-    //   const writeStream = fs.createWriteStream(outputGzPath);
-    //   const bufferStream = require("stream").Readable.from([resultJson]);
+    await new Promise((resolve, reject) => {
+      const gzip = zlib.createGzip();
+      const writeStream = fs.createWriteStream(outputGzPath);
+      const bufferStream = require("stream").Readable.from([resultJson]);
 
-    //   bufferStream.pipe(gzip).pipe(writeStream).on("finish", resolve).on("error", reject);
-    // });
+      bufferStream.pipe(gzip).pipe(writeStream).on("finish", resolve).on("error", reject);
+    });
+    return resultObject;
     // let newblastn = new Blastn({
     //   url: inputFilePath,
     //   code: id,
