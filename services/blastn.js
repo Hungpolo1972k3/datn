567cc889fc29ac872e423aa553fe502d000029e7
Hung <hungnguyen1972k3@gmail.com>
2025-06-10 21:33:30 +0700

    update blastn

--- services/blastn.js
+++ services/blastn.js
@@ -233,20 +233,20 @@ const runBlastnTool = async (inputFilePath, filename, id) => {
       };
     });
     const resultObject = {
-      virulence: virulenceDocs,
-      amr: amrDocs
+      virulence: virulence,
+      amr: amr
     };
-    const resultJson = JSON.stringify(resultObject, null, 2);
-    const fastaDir = path.join('/app', 'Blastn');
-    const outputGzPath = path.join(fastaDir, `${id}_result.json.gz`);
+    // const resultJson = JSON.stringify(resultObject, null, 2);
+    // const fastaDir = path.join('/app', 'Blastn');
+    // const outputGzPath = path.join(fastaDir, `${id}_result.json.gz`);
 
-    await new Promise((resolve, reject) => {
-      const gzip = zlib.createGzip();
-      const writeStream = fs.createWriteStream(outputGzPath);
-      const bufferStream = require("stream").Readable.from([resultJson]);
+    // await new Promise((resolve, reject) => {
+    //   const gzip = zlib.createGzip();
+    //   const writeStream = fs.createWriteStream(outputGzPath);
+    //   const bufferStream = require("stream").Readable.from([resultJson]);
 
-      bufferStream.pipe(gzip).pipe(writeStream).on("finish", resolve).on("error", reject);
-    });
+    //   bufferStream.pipe(gzip).pipe(writeStream).on("finish", resolve).on("error", reject);
+    // });
     return resultObject;
     // let newblastn = new Blastn({
     //   url: inputFilePath,
