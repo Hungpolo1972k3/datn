5363690fbd58010afca7fb0ed9a4fe353026fc56
Hung <hungnguyen1972k3@gmail.com>
2025-06-10 21:50:19 +0700

    update blastn

--- services/blastn.js
+++ services/blastn.js
@@ -12,8 +12,8 @@ const dataDir = path.join('/app', 'FastA');
 // const dataDir = path.join(__dirname,"../../FastA")
 
 const runBlastnTool = async (inputFilePath, filename, id) => {
-  const form = new FormData();
-  form.append('fasta', fs.createReadStream(inputFilePath)); 
+  // const form = new FormData();
+  // form.append('fasta', fs.createReadStream(inputFilePath)); 
   try {
     // const response = await axios.post(
     //   `${process.env.BIOTOOL_URL}/api/blastn/blastn/${id}`,
