e74c37f822e76b39bb75cffcc3545cdb9d59362d
Hung <hungnguyen1972k3@gmail.com>
2025-06-10 21:01:10 +0700

    update blastn

--- services/blastn.js
+++ services/blastn.js
@@ -24,8 +24,8 @@ const runBlastnTool = async (inputFilePath, filename, id) => {
     //     },
     //   }
     // );
-    const virulence = await virulenceService.runVirulenceTool({ path: inputFilePath });
-    const amr = await amrService.runAmrTool({ path: inputFilePath });
+    const virulence = await virulenceService.runVirulenceTool2(inputFilePath);
+    const amr = await amrService.runAmrTool2(inputFilePath);
     const resultObject = {
       virulence: virulence.data,
       amr: amr.data
